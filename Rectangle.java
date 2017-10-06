b4e928b93dbfb73ada2be54e8d66ddd54fb458ca
sriti7 <sritiahmed07@gmail.com>
2017-10-06 01:27:00 -0400

    initial commit of rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,45 @@
+
+
+import javax.swing.*;
+
+
+
+
+
+
+
+public class Rectangle {
+
+     public static void main (String [] args) {
+     
+         System.out.println( "Hello out there");
+        
+	  
+
+	 int width = 64;
+	 int height =24;
+
+
+	 String input1 = JOptionPane.showInputDialog("Enter the width of Rectangle");
+	 String input2= JOptionPane.showInputDialog("Enter the height of Rectangle");
+	 JOptionPane.showMessageDialog (null, "The area of Rectangle is " + width*height);
+	 JOptionPane.showMessageDialog (null, "The perimeter of Rectangle is " + 2*width + 2*height);
+
+
+
+
+
+
+     
+     
+     
+     
+     
+     
+     
+     }
+
+
+
+
+}
